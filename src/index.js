fb66f515e526471dc71eeb1f443cd792e59772a4
Jeremy Wells <jemmyw@gmail.com>
2016-03-03 16:52:24 +0100

    Send error if createSound fails

--- src/index.js
+++ src/index.js
@@ -114,10 +114,17 @@ function createSound(obs, command) {
     whileplaying: () => soundEvent(thisSound, obs, `playing`),
     onfailure: (err) => soundEvent(thisSound, obs, `failure`, err),
   })
-  thisSound.scope = command.scope
 
-  sounds[thisSound.id] = thisSound
-  return thisSound
+  if(thisSound) {
+    thisSound.scope = command.scope
+    sounds[thisSound.id] = thisSound
+  } else {
+    soundError({
+      scope: command.scope,
+      id: null,
+      url: command.src
+    }, obs);
+  }
 }
 
 function performCommand(obs, command) {
